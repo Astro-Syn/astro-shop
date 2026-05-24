dc77fd8802bba1a23537ba396644efc44c8e5b9c
Astro-Syn <kelseyamyb@gmail.com>
2026-05-24 11:36:01 -0400

    add footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  showAbout = false;
 
+  openAbout(){
+    this.showAbout = true;
+  }
+
+  closeAbout(){
+    this.showAbout = false;
+  }
 }
